7b3c05b6636f23afd46d2a4eaf83797494d3426b
fellettigiacomo <g.felletti@icloud.com>
2024-07-10 02:10:43 +0200

    updated fw

--- wled00/wled.h
+++ wled00/wled.h
@@ -8,8 +8,8 @@
  */
 
 // version code in format yymmddb (b = daily build)
-#define VERSION 2405180
-
+#define VERSION 2407100
+#define USERMOD_MULTI_RELAY
 //uncomment this if you have a "my_config.h" file you'd like to use
 //#define WLED_USE_MY_CONFIG
 
